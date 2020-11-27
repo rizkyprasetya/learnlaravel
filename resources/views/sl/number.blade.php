860635aeaadc8d332e8ababe801c936a6cfd026a
rizkyprasetya <rizky-milis@rea-prasetya.net>
2020-11-28 00:29:04 +0700

    hoho

--- resources/views/sl/number.blade.php
+++ resources/views/sl/number.blade.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Second Largest</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <form action="/sl-number" method="post">
         @csrf
@@ -14,4 +20,5 @@
         <button type="submit">submit</button>
     </form>
 </body>
+
 </html>
--- resources/views/sl/vector.blade.php
+++ resources/views/sl/vector.blade.php
@@ -1,22 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Input Vector</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <form action="sl-cal" method="post">
         @csrf
         <table>
             @for ($i = 1; $i <= $number; $i++)
-            <tr>
-                <td>Masukkan data ke {{$i}}</td><td><input type="text" name="data[{{$i}}]" id="data-{{$i}}"></td>
-            </tr>                
-            @endfor          
+                <tr>
+                    <td>Masukkan data ke {{ $i }}</td>
+                    <td><input type="text" name="data[{{ $i }}]" id="data-{{ $i }}"></td>
+                </tr>
+            @endfor
         </table>
         <button type="submit">Submit</button>
     </form>
 </body>
+
 </html>
